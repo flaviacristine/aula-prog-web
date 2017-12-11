9ca9d15afc78df5d11c73bf56da9e8344f889db7
José Francisco de Oliveira Junior <jose.francisco@engcomp.local>
2017-12-11 12:42:15 -0200

    CRUD básico de Unidade.

---
+++ src/main/java/br/edu/unievangelica/domain/unidade/UnidadeController.java
@@ -0,0 +1,46 @@
+package br.edu.unievangelica.domain.unidade;
+
+import br.edu.unievangelica.core.controller.ResponseAbstractController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/unidade")
+public class UnidadeController extends ResponseAbstractController {
+
+    @Autowired
+    UnidadeService unidadeService;
+
+    @GetMapping
+    public ResponseEntity<?> findAll(){
+        return jsonResponse(unidadeService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> findById(@PathVariable long id){
+        Unidade unidade = unidadeService.findOnde(id);
+        return jsonResponse(unidade);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@Validated @RequestBody Unidade unidade){
+        return jsonResponse(unidadeService.save(unidade));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@Validated @RequestBody Unidade unidade){
+        return jsonResponse(unidadeService.save(unidade));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id){
+        unidadeService.delete(id);
+        return jsonResponse(null);
+    }
+
+
+
+}
---
+++ src/main/java/br/edu/unievangelica/domain/unidade/UnidadeService.java
@@ -0,0 +1,8 @@
+package br.edu.unievangelica.domain.unidade;
+
+import br.edu.unievangelica.core.service.AbstractService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnidadeService extends AbstractService<Unidade> {
+}
