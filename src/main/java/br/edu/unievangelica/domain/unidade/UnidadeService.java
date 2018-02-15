520a5e8beb25edaa98bde6b4d93c9c1318941655
José Francisco de Oliveira Junior <jose.francisco@engcomp.local>
2018-02-15 12:17:10 -0300

    Validações de campos já cadastrados ok.

--- src/main/java/br/edu/unievangelica/domain/unidade/UnidadeService.java
+++ src/main/java/br/edu/unievangelica/domain/unidade/UnidadeService.java
@@ -1,7 +1,7 @@
 package br.edu.unievangelica.domain.unidade;
 
-import br.edu.unievangelica.core.controller.JsonResponse;
 import br.edu.unievangelica.core.enums.SituacaoEnum;
+import br.edu.unievangelica.core.exception.GenericException;
 import br.edu.unievangelica.core.service.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,5 +25,14 @@ public class UnidadeService extends AbstractService<Unidade> {
         if (!unidades.isEmpty())
             return new ResponseEntity<String>(HttpStatus.OK);
         return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
+    }
+
+    @Override
+    public Unidade save(Unidade unidade) throws GenericException {
+        List<Unidade> unidades = unidadeRepository.findUnidadeByNomeLike(unidade.getNome(), unidade.getId());
+        if (!unidades.isEmpty()){
+            return null;
+        }
+        return super.save(unidade);
     }
 }
