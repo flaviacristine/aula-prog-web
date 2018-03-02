e73e86ab96ff20fb8ee7d5e05c4f487ee36c69e1
jose.francisco <jose.franciscoj@outlook.com>
2018-03-02 12:14:20 -0300

    Teste finalizado.

--- src/main/java/br/edu/unievangelica/domain/Unidade.java
+++ src/main/java/br/edu/unievangelica/domain/Unidade.java
@@ -28,27 +28,31 @@ public class Unidade implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "unidade_id_seq")
-    @SequenceGenerator( name = "unidade_id_seq", sequenceName = "unidade_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "unidade_id_seq")
+    @SequenceGenerator(name = "unidade_id_seq", sequenceName = "unidade_id_seq", allocationSize = 1)
     @Column(name = "id")
     @Getter
+    @Setter
     protected long id;
 
     @NotEmpty
     @Size(max = 10)
-    @Column(name = "codigo", updatable= false)
-    @Getter @Setter
+    @Column(name = "codigo", updatable = false)
+    @Getter
+    @Setter
     private String codigo;
 
     @NotEmpty
     @Size(max = 80)
     @Column(name = "nome")
-    @Getter @Setter
+    @Getter
+    @Setter
     private String nome;
 
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "situacao")
-    @Getter @Setter
+    @Getter
+    @Setter
     private SituacaoEnum situacao = SituacaoEnum.ATIVO;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "unidade")
--- src/test/java/br/edu/unievangelica/domain/unidade/UnidadeControllerTest.java
+++ src/test/java/br/edu/unievangelica/domain/unidade/UnidadeControllerTest.java
@@ -3,9 +3,9 @@ package br.edu.unievangelica.domain.unidade;
 import br.edu.unievangelica.VirtooApplication;
 import br.edu.unievangelica.core.enums.SituacaoEnum;
 import br.edu.unievangelica.domain.Unidade;
+import br.edu.unievangelica.domain.UnidadeService;
 import br.edu.unievangelica.domain.instituicao.Instituicao;
 import br.edu.unievangelica.domain.instituicao.InstituicaoService;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.After;
 import org.junit.Before;
@@ -17,17 +17,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
@@ -38,13 +34,8 @@ public class UnidadeControllerTest {
 
     @Autowired
     WebApplicationContext webApplicationContext;
-    private MockMvc mockMvc;
-
-    JsonPath unidade;
 
     private List<Unidade> unidades;
-
-    public InstituicaoService instituicaoService;
 
 
     @Before
@@ -59,19 +50,22 @@ public class UnidadeControllerTest {
     }
 
     @Test
-    public void listarComListaVazia(){}
-
+    public void listarComListaVazia(){
+        get("http://localhost:8181/api/unidade")
+                .then()
+                .statusCode(200)
+                .body(String.valueOf("content".toString()), hasToString("[]"));
+    }
 
     @Test
     public void cadastrarItemComDadosValidos(){
-        Unidade unidade = new Unidade();
-        unidade.setNome("Unidade 1");
-        unidade.setCodigo("UN1");
-        unidade.setSituacao(SituacaoEnum.ATIVO);
-
         Instituicao instituicao = new Instituicao();
         instituicao.setId(1);
 
+        Unidade unidade = new Unidade();
+        unidade.setNome("Unidade 5");
+        unidade.setCodigo("UN5");
+        unidade.setSituacao(SituacaoEnum.ATIVO);
         unidade.setInstituicao(instituicao);
 
         given()
@@ -84,18 +78,34 @@ public class UnidadeControllerTest {
     }
 
     @Test
-    public void cadastrarItemComDadosInvalidos(){}
+    public void cadastrarItemComDadosInvalidos(){
+        Instituicao instituicao = new Instituicao();
+        instituicao.setId(2);
+
+        Unidade unidade = new Unidade();
+        unidade.setNome("Unidade 5");
+        unidade.setCodigo("UN555555555");
+        unidade.setSituacao(SituacaoEnum.ATIVO);
+        unidade.setInstituicao(instituicao);
+
+        given()
+                .contentType("application/json")
+                .body(unidade)
+                .when()
+                .post("http://localhost:8181/api/unidade")
+                .then()
+                .statusCode(400);
+    }
 
     @Test
     public void cadastrarItemComDadosDuplicados(){
+        Instituicao instituicao = new Instituicao();
+        instituicao.setId(1);
+
         Unidade unidade = new Unidade();
         unidade.setNome("Unidade 1");
         unidade.setCodigo("UN1");
         unidade.setSituacao(SituacaoEnum.ATIVO);
-
-        Instituicao instituicao = new Instituicao();
-        instituicao.setId(1);
-
         unidade.setInstituicao(instituicao);
 
         given()
@@ -107,22 +117,19 @@ public class UnidadeControllerTest {
                 .statusCode(500);
     }
 
-
     @Test
     public void listarTodosItens(){
         get("http://localhost:8181/api/unidade")
                 .then()
-                .statusCode(200)
-                .getClass().equals(Unidade.class);
+                .statusCode(200);
     }
-
 
     @Test
     public void buscarItemExistentePeloID() {
-        get("http://localhost:8181/api/unidade/{id}", 2)
+        get("http://localhost:8181/api/unidade/{id}", 6)
                 .then()
                 .statusCode(200)
-                .getClass().equals(Unidade.class);
+                .getClass();
     }
 
     @Test
@@ -132,15 +139,68 @@ public class UnidadeControllerTest {
                 .statusCode(500);
     }
 
+    @Test
+    public void alterarItemCadastradoComDadosValidos(){
+        Instituicao instituicao = new Instituicao();
+        instituicao.setId(1);
+
+        Unidade unidade = new Unidade();
+        unidade.setId(6);
+        unidade.setNome("Unidade 2");
+        unidade.setCodigo("UN1");
+        unidade.setSituacao(SituacaoEnum.ATIVO);
+        unidade.setInstituicao(instituicao);
+
+        given()
+                .contentType("application/json")
+                .body(unidade)
+                .when()
+                .put("http://localhost:8181/api/unidade")
+                .then()
+                .statusCode(200);
+    }
 
     @Test
-    public void alterarItemCadastradoComDadosValidos(){}
+    public void alterarItemCadastradoComDadosInvalidos(){
+        Instituicao instituicao = new Instituicao();
+        instituicao.setId(1);
+
+        Unidade unidade = new Unidade();
+        unidade.setId(6);
+        unidade.setNome("Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / Unidade 2 / ");
+        unidade.setCodigo("UN1");
+        unidade.setSituacao(SituacaoEnum.ATIVO);
+        unidade.setInstituicao(instituicao);
+
+        given()
+                .contentType("application/json")
+                .body(unidade)
+                .when()
+                .post("http://localhost:8181/api/unidade")
+                .then()
+                .statusCode(400);
+    }
 
     @Test
-    public void alterarItemCadastradoComDadosInvalidos(){}
+    public void alterarItemCadastradoComDadosDuplicados(){
+        Instituicao instituicao = new Instituicao();
+        instituicao.setId(1);
 
-    @Test
-    public void alterarItemCadastradoComDadosDuplicados(){}
+        Unidade unidade = new Unidade();
+        unidade.setId(6);
+        unidade.setNome("Unidade 1");
+        unidade.setCodigo("UN1");
+        unidade.setSituacao(SituacaoEnum.ATIVO);
+        unidade.setInstituicao(instituicao);
+
+        given()
+                .contentType("application/json")
+                .body(unidade)
+                .when()
+                .put("http://localhost:8181/api/unidade")
+                .then()
+                .statusCode(500);
+    }
 
     @Test
     public void alterarItemInexistente(){
@@ -149,143 +209,29 @@ public class UnidadeControllerTest {
                 .statusCode(404);
     }
 
-
     @Test
     public void excluirItemCadastrado(){
-        delete("http://localhost:8181/api/unidade/{id}", 1)
+        delete("http://localhost:8181/api/unidade/{id}", 5)
                 .then()
                 .statusCode(200);
 
-        get("http://localhost:8181/api/unidade/{id}", 1)
+        get("http://localhost:8181/api/unidade/{id}", 5)
                 .then()
-                .statusCode(404);
+                .statusCode(500);
     }
 
     @Test
-    public void excluirItemComDependencia(){}
+    public void excluirItemComDependencia(){
+        delete("http://localhost:8181/api/unidade/{id}", 7)
+                .then()
+                .statusCode(500);
+    }
 
     @Test
     public void excluirItemInexistente(){
         delete("http://localhost:8181/api/unidade/{id}", 1)
                 .then()
                 .statusCode(404);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Test
-    public void findAll() throws Exception {
-        get("http://localhost:8181/api/unidade").
-                then().
-                statusCode(200).
-                and().
-                getClass().
-                equals(Unidade.class);
-    }
-
-    @Test
-    public void verificarCamposContidosEmUnidade() throws Exception {
-
-        unidades = get("http://localhost:8181/api/unidade").getBody().jsonPath().getList("unidades", Unidade.class);
-
-        assertThat(unidades, everyItem(hasProperty("id", notNullValue())));
-        assertThat(unidades, everyItem(hasProperty("nome", notNullValue())));
-        assertThat(unidades, everyItem(hasProperty("codigo", notNullValue())));
-        assertThat(unidades, everyItem(hasProperty("situacao", not(empty()))));
-        assertThat(unidades, everyItem(hasProperty("arquivo", isEmptyOrNullString())));
-        assertThat(unidades, everyItem(hasProperty("instituicao", not(empty()))));
-    }
-
-    @Test
-    public void buscarPorId() throws Exception {
-
-        get("http://localhost:8181/api/unidade/{id}", 2).
-                then().
-                statusCode(200).
-                body("id", notNullValue()).
-                body("nome", notNullValue()).
-                body("codigo", notNullValue()).
-                body("situacao", notNullValue()).
-                body("arquivo", isEmptyOrNullString()).
-                body("instituicao", notNullValue()).
-                getClass().equals(Unidade.class);
-    }
-
-    @Test
-    public void verificarCodigoJaCadastradoConflict() throws Exception {
-        get("http://localhost:8181/api/unidade/find-codigo/{id}", 2).
-                then().
-                statusCode(409);
-    }
-
-    @Test
-    public void verificarCodigoJaCadastradoOk() throws Exception {
-        get("http://localhost:8181/api/unidade/find-codigo/{id}", 12345).
-                then().
-                statusCode(200);
-    }
-
-    @Test
-    public void verificarNomeJaCadastradoConflict() throws Exception {
-        get("http://localhost:8181/api/unidade/find-nome/unidade 2").
-                then().
-                statusCode(409);
-    }
-
-    @Test
-    public void verificarNomeJaCadastradoOk() throws Exception {
-        get("http://localhost:8181/api/unidade/find-nome/unidade123").
-                then().
-                statusCode(200);
-    }
-
-    @Test
-    public void salvarEmJason() throws Exception {
-
-        Unidade unidade = new Unidade();
-        unidade.setNome("Unidade 1");
-        unidade.setCodigo("UN1");
-        unidade.setSituacao(SituacaoEnum.ATIVO);
-
-        Instituicao instituicao = new Instituicao();
-        instituicao.setId(1);
-
-        unidade.setInstituicao(instituicao);
-
-        given()
-            .contentType("application/json")
-            .body(unidade)
-        .when()
-            .post("http://localhost:8181/api/unidade")
-        .then()
-            .statusCode(200);
-    }
-
-    @Test
-    public void apagaAtorExistente() {
-
-        delete("http://localhost:8181/api/unidade/{id}", 1).
-            then().
-            statusCode(200);
-
-        get("http://localhost:8181/api/unidade/{id}", 1)
-            .then()
-            .statusCode(404);
     }
 
 }
